2a942d4a9a6fd071428dec3be754f0a29e65d14a
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-04-18 22:13:40 -0300

    Anos de Vidas do cachorro

---
+++ dias do ano/index.js
@@ -0,0 +1,5 @@
+var myAge = 20;
+var firstYears = 2 * 10.5
+var latterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + latterYears;
+console.log('My age in dog years is' +myAgeInDogYears+'.');
