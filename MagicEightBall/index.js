e4f7cc4ee2cd60b6f79fa0c608ed740dd5f22438
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-04-19 22:35:25 -0300

    MagicEightBall

---
+++ MagicEightBall/index.js
@@ -0,0 +1,38 @@
+let userName = 'Ela';
+
+userName ? console.log(`Hello, ${userName}.`) : console.log('Hello');
+
+
+let userQuestion = 'Will I do this exercise?';
+let randomNumber = Math.floor(Math.random() * 7);
+let eightBall = '';
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidely so';
+        break;
+    case 2 : 
+    eightBall = 'Reply hazy try again';
+    break;
+    case 3 : 
+    eightBall = 'Cannot predict now';
+    break;
+    case 4: 
+    eightBall  = 'Dont count on it';
+    break;
+    case 5 :
+        eigthBall = 'My souces say no';
+        break;
+        case 6:
+            eightBall = 'Outlook not so good';
+            break;
+            case 7 :
+                eightBall = 'Signs point to yes';
+                break;
+}
+
+console.log('User question: ' + userQuestion );
+console.log('The eight ball answer: ' + eightBall);
